82ed5baa57593d011683864bb466cde08a6f088e
OrIvan611 <92181597+OrIvan611@users.noreply.github.com>
2021-12-04 20:10:07 +0200

    Add files via upload

---
+++ Philosophy Challenge/consts.py
@@ -0,0 +1,20 @@
+# Wikipedia consts.
+WIKIPEDIA_URL_START = 'https://en.wikipedia.org/wiki/'
+# FIRST_URL = WIKIPEDIA_URL_START + 'Special:Random'
+FIRST_URL = WIKIPEDIA_URL_START + 'Special:Random'
+TARGET_VALUE = 'Philosophy'
+
+# Program consts.
+ITERATIONS = 1000
+
+# requests consts.
+OK_STATUS = 200
+
+# BeautifulSoup consts.
+PARAGRAPH_SIGN = 'p'
+LINK_SIGN = 'a'
+HTML_PARSER = "html.parser"
+HTML_TITLE = "firstHeading"
+
+# Networkx consts.
+NODE_SIZE = 5000
