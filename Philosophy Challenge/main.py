2e79629bcc9ac94839a67f29df799fa37a7b904f
OrIvan611 <92181597+OrIvan611@users.noreply.github.com>
2021-12-04 20:24:54 +0200

    Add files via upload

---
+++ Philosophy Challenge/main.py
@@ -0,0 +1,34 @@
+from utils import *
+from consts import *
+
+
+used_urls = []
+url = FIRST_URL
+for i in range(ITERATIONS):
+    if url in used_urls:
+        # We already visited this page.
+        print('I am in a loop...')
+        break
+    used_urls.append(url)
+
+    if check_if_title_match(url):
+        # We found the requested ulr!
+        print('i got it')
+        break
+
+    url = get_new_url(url)
+    print(url)
+    if not url:
+        # The previous url had no valid links in it.
+        print('There are no links in this page.')
+        break
+else:
+    # We didn't find the requested url, and we got to the ITERATION limit.
+    print(f'I tried {ITERATIONS} times..')
+
+# Plot the graph without the random node.
+plot_graph(used_urls[1:])
+
+
+
+
