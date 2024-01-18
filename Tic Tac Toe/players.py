d9879892201c354b91b7079b29865d32340b214c
danla <dan.lasker12@gmail.com>
2024-01-18 18:20:15 +0200

    dan comments

--- Tic Tac Toe/players.py
+++ Tic Tac Toe/players.py
@@ -1,10 +1,11 @@
-
+# TODO : remove this module
 class Players:
     def __init__(self, name, sign):
         self.name = name
         self.sign = sign
 
     def play(self, board):
+        # TODO: Why so many Play*()
         location = int(input(f'{self.name}, Pick a place between 0-8: '))
 
         board[int(location/3)][location%3] = self.sign
--- tic tac toe/main.py
+++ tic tac toe/main.py
@@ -11,7 +11,10 @@ if the board is full, but no one won.
 from Player import Player
 from Board import Board
 from Play import Play
-import check_board
+import check_board  # USE directly from .. .import ..
+
+
+# Create a new class that handles that logic and the engine of the game
 
 
 def play(board: Board, player1: Player, player2: Player):
@@ -28,7 +31,9 @@ def play(board: Board, player1: Player, player2: Player):
             player2.is_playing = False
 
 
+#TODO: Names of functions have to be verbs!
 def game_over_message(player1: Player, player2: Player, game_over_reason: str) -> str:
+    # FINd a way to shorten your code!
     if player1.is_playing:
         winner = player2.name
     else:
@@ -41,26 +46,29 @@ def game_over_message(player1: Player, player2: Player, game_over_reason: str) -
     return message
 
 
+#todo: I recommend you to put this inside the Board class
 def check_board_status(board: Board, player1: Player, player2: Player) -> bool:
-    line = check_board.CheckLine(board.board)
+    line = check_board.CheckLine(board.board)  # TODO:
     column = check_board.CheckColumn(board.board)
     diagonal = check_board.CheckDiagonal(board.board)
     tie = check_board.CheckTie(board.board)
 
     for game_over_reason in [line, column, diagonal, tie]:
         if game_over_reason.check():
-            print(board.print_board())
+            print(board.print_board())  # TODO: Do it only in one line
             print(game_over_message(player1, player2, str(game_over_reason)))
             return True
     return False
 
 
 if __name__ == '__main__':
+    # TODO: Shorten your main!
     board = Board(3, 3)
     player1 = Player('or', 'X', board.board, True)
     player2 = Player('mich', 'O', board.board, False)
     game_over = check_board_status(board, player1, player2)
 
+    #todo: Put it out of the main
     while not game_over:
         play(board, player1, player2)
         game_over = check_board_status(board, player1, player2)
